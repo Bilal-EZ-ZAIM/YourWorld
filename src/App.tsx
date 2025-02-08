4ae5b1971434ae588980808a4ff29dd37bf62a2d
Bilal Ezzaim <bilalzaim499@gmail.com>
2025-02-08 15:36:43 +0000

    predeploy

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const isAuthenticated = false; // This will be connected to authentication state later
 
   return (
-    <Router>
+    <Router basename="/YourWorld">
       <Routes>
         {/* <Route path="/" element={isAuthenticated ? <ChatLayout /> : <Login />} /> */}
         <Route path="/" element={<ChatLayout />} />
